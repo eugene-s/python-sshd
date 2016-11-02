96ef83e94dc509746ec6c21584440362c7df3a7a
Eugene Savchenko <savchenko.eo@gmail.com>
2016-11-02 20:06:18 +0200

    Improves

--- python-3.5-alpine/rootfs/usr/local/bin/entrypoint.sh
+++ python-3.5-alpine/rootfs/usr/local/bin/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/ash
 
+DAEMON=sshd
+
 # Generate Host keys, if required
 if ! ls /etc/ssh/host_keys/ssh_host_* 1> /dev/null 2>&1; then
     ssh-keygen -A
@@ -14,5 +16,25 @@ if ! ls /root/env 1> /dev/null 2>&1; then
     virtualenv /root/venv/
 fi
 
-# do not detach (-D), log to stderr (-e), passthrough other arguments
-exec /usr/sbin/sshd -D -e "$@"
+stop() {
+    echo -e "\nReceived SIGINT or SIGTERM. Shutting down ${DAEMON}"
+    # Get PID
+    pid=$(cat /var/run/${DAEMON}/${DAEMON}.pid)
+    # Set TERM
+    kill -SIGTERM "${pid}"
+    # Wait for exit
+    wait "${pid}"
+    # All done.
+    echo "Done."
+}
+
+if [ "$(basename $1)" == "${DAEMON}" ]; then
+    echo "Running $@"
+    trap stop SIGINT SIGTERM
+    $@ &
+    pid="$!"
+    mkdir -p /var/run/${DAEMON} && echo "${pid}" > /var/run/${DAEMON}/${DAEMON}.pid
+    wait "${pid}" && exit $?
+else
+    exec "$@"
+fi
